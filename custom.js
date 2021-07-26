ba37aac8c0e0f620b62779fa2d83722d8a379583
engrasel <contact.engrasel@gmail.com>
2021-07-26 17:02:41 +0600

    Phone increase and decrease Fully functional

--- custom.js
+++ custom.js
@@ -1,42 +1,73 @@
+/*---------------- Phone Even Handler start ----------------*/ 
+
 // phone-increase even handler
 document.getElementById('phone-increase').addEventListener('click', function () {
-    const PhoneInput = document.getElementById('phone-count').value;
-    const PhoneCount = parseInt(PhoneInput);
-    const newPhoneCount = PhoneCount + 1;
-    document.getElementById('phone-count').value = newPhoneCount;
-
-    const PhoneTotall = newPhoneCount * 1219;
-    document.getElementById('phone-totall').innerText = PhoneTotall;
+    handlePhoneChange(true);
 });
 
 // phone-decrease even handler
 document.getElementById('phone-decrease').addEventListener('click', function () {
+    handlePhoneChange(false);
+});
+
+function handlePhoneChange(isIncrease) {
     const PhoneInput = document.getElementById('phone-count').value;
     const PhoneCount = parseInt(PhoneInput);
-    const newPhoneCount = PhoneCount - 1;
+    // const newPhoneCount = PhoneCount - 1; // const newPhoneCount = PhoneCount + 1;
+    let newPhoneCount = PhoneCount; //initial value
+    if (isIncrease == true) {
+        newPhoneCount = PhoneCount + 1;
+    }
+    if (isIncrease == false && PhoneCount > 0) {
+        newPhoneCount = PhoneCount - 1;
+    }
     document.getElementById('phone-count').value = newPhoneCount;
 
     const PhoneTotall = newPhoneCount * 1219; //per unit price
     document.getElementById('phone-totall').innerText = PhoneTotall;
-});
+}
+
+// // phone-increase even handler
+// document.getElementById('phone-increase').addEventListener('click', function () {
+//     const PhoneInput = document.getElementById('phone-count').value;
+//     const PhoneCount = parseInt(PhoneInput);
+//     const newPhoneCount = PhoneCount + 1; 
+//     document.getElementById('phone-count').value = newPhoneCount;
+
+//     const PhoneTotall = newPhoneCount * 1219;
+//     document.getElementById('phone-totall').innerText = PhoneTotall;
+// });
+
+// // phone-decrease even handler
+// document.getElementById('phone-decrease').addEventListener('click', function () {
+//     const PhoneInput = document.getElementById('phone-count').value;
+//     const PhoneCount = parseInt(PhoneInput);
+//     const newPhoneCount = PhoneCount - 1;
+//     document.getElementById('phone-count').value = newPhoneCount;
+
+//     const PhoneTotall = newPhoneCount * 1219; //per unit price
+//     document.getElementById('phone-totall').innerText = PhoneTotall;
+// });
 
 
+
+/*---------------- Case Even Handler start ----------------*/ 
 
 // Case-increase even handler
 document.getElementById('case-increase').addEventListener('click', function () {
-    handleProductChange(true);
+    handleCaseChange(true);
 });
 
 // Case-decrease even handler
 document.getElementById('case-decrease').addEventListener('click', function () {
-    handleProductChange(false);
+    handleCaseChange(false);
 });
 
-function handleProductChange(isIncrease) {
+function handleCaseChange(isIncrease) {
     const CaseInput = document.getElementById('case-count').value;
     const CaseCount = parseInt(CaseInput);
     // const newCaseCount = CaseCount - 1; // const newCaseCount = CaseCount + 1;
-    let newCaseCount = CaseCount;
+    let newCaseCount = CaseCount; //initial value
     if (isIncrease == true) {
         newCaseCount = CaseCount + 1;
     }
@@ -70,3 +101,5 @@ function handleProductChange(isIncrease) {
 //     const CaseTotall = newCaseCount * 59; //per unit price
 //     document.getElementById('case-totall').innerText = CaseTotall;
 // });
+
+/*---------------- Case Even Handler end ----------------*/ 
